99901208b70d36dbaeac735650062a1fde262b19
Vladislav Chervyakov <chervyakovve@mail.ru>
2026-05-11 00:23:33 +0700

    fix

--- src/modules/github/github.service.ts
+++ src/modules/github/github.service.ts
@@ -110,7 +110,7 @@ export const createPullRequestReview = async (
       const summaryWithComments = [
         review.summary,
         '\n\n### 💡 Детальные замечания:',
-        ...review.comments.map((c) => `* **${c.file}:${c.line}**: ${c.comment}`),
+        ...review.reviews.map((c) => `* **${c.file}:${c.line}**: ${c.comment}`),
       ].join('\n');
 
       return await githubProvider.post(`${prUrl}/reviews`, {
